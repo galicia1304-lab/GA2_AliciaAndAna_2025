e7f0a8628a42788fa95745bf81565b48f5df88a6
johnklima <klima.de.sa@gmail.com>
2025-09-25 16:23:42 +0200

    little fix

--- Assets/Scripts/DropPaper.cs
+++ Assets/Scripts/DropPaper.cs
@@ -1,30 +1,29 @@
 using UnityEngine;
 
-public class PaperPickup : MonoBehaviour
+public class DropPaper : MonoBehaviour
 {
     private Vector3 originalPosition;
     private Quaternion originalRotation;
     private Transform originalParent;
 
-    private bool isHeld = false;
-    public Transform holdPoint; // Empty GameObject in front of the player where paper will appear
-
+    public bool isHeld = false;
+   
     void Start()
     {
         // Save where the paper starts
-        originalPosition = transform.position;
-        originalRotation = transform.rotation;
+        originalPosition = transform.localPosition;
+        originalRotation = transform.localRotation;
         originalParent = transform.parent;
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.E) )
         {
             // Put paper back in drawer
             transform.SetParent(originalParent);
-            transform.position = originalPosition;
-            transform.rotation = originalRotation;
+            transform.localPosition = originalPosition;
+            transform.localRotation = originalRotation;
             isHeld = false;
 
         }
